aab4a0cd94f8aee6aadc9ad4ff79d2b4632c401d
Joseph Carey <josephcareycomposer@gmail.com>
2018-08-31 15:22:45 -0500

    initial commit, partially completed.

---
+++ scripts/client.js
@@ -0,0 +1,19 @@
+console.log('js');
+
+class Employee {
+    constructor (firstName, lastName, id, title, annualSalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.title = title;
+        this.annualSalary = annualSalary;
+    }
+    
+}
+
+$('document').ready( readyNow );
+
+function readyNow () {
+    console.log('jQ');
+    
+}
